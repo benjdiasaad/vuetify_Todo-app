27041d12cc63118bcd2179276a6645a1c26298ff
Benjdia Saad <benjdiasaad97@gmail.com>
2021-08-03 22:30:25 +0100

    create app vue js

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,52 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list-item>
+        <v-list-item-content>
+          <v-list-item-title class="text-h6"> Application </v-list-item-title>
+          <v-list-item-subtitle> Best Todo Ever! </v-list-item-subtitle>
+        </v-list-item-content>
+      </v-list-item>
+
+      <v-divider></v-divider>
+
+      <v-list dense nav>
+        <v-list-item 
+        v-for="item in items" 
+        :key="item.title" 
+        :to="item.to"
+        link>
+          <v-list-item-icon>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-icon>
+
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Benjdia </v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <router-view> </router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  data: () => ({
+    drawer: null,
+    items: [
+      { title: "Todo", icon: "mdi-format-list-checks", to : '/' },
+      { title: "About", icon: "mdi-help-box", to : '/about' },
+    ],
+  }),
+};
+</script>
---
+++ src/views/Todo.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="home pa-6">
+    <h1> Todo app</h1>
+  </div>
+</template>
+
+<script>
+
+  export default {
+    name: 'Home',
+  }
+</script>
